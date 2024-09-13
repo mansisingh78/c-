dfd77597c75e5f7cecbc640eee40afbb477b3e41
Mansi singh <mansi123singh1234@gmail.com>
2024-09-13 07:59:06 +0530

    completed

--- loop.cpp
+++ loop.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 int main()
 {
-    // while loop
-    // int count = 1;
-    // int n = 50;
 
-    // while (count <= n)
-    // {
-    //     cout << count << " ";
-    //     count++;
-    // }
+    // while loop
+    int count = 1;
+    int n = 50;
+
+    while (count <= n)
+    {
+        cout << count << " ";
+        count++;
+    }
     // for loop
 
-    // int n = 5;
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     cout << i << " ";
-    // }
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
     // sum of numbers from 1 to n.
 
     int n = 3;
@@ -28,5 +29,32 @@ int main()
     }
     cout << "sum = " << sum << endl;
 
+    return 0;
+
+    // sum of all odd numbers 1 to n
+
+    int n = 10;
+    int oddSum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            oddSum += i;
+        }
+    }
+    cout << "odd sum = " << oddSum << endl;
+    // nested loops
+
+    int n = 10;
+    for (int i = 1; i <= n; i++)
+    {
+        int m = 5;
+        for (int j = 1; j <= m; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
